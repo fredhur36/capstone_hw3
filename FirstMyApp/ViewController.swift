3bb62c074843f0739a62b13d23346c51207bd346
fred <fredhur36@junhyeong-ui-mac-mini.local>
2018-03-25 00:54:10 +0900

    new commit

--- FirstMyApp/ViewController.swift
+++ FirstMyApp/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+  //  label.adjustsFontSizeToFitWidth = true
+    let messageArray = ["May the force be with you","Live long and prosepr", "To infinitny and beynod","Space is big. You just won't believe how vastly, hugely, mindbogglinly big it is",10] as [Any]
+    var index = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func doButtonTab(_ sender: UIButton) {
+        print("Button touched")
+        if let nextString = self.messageArray[index] as? String {// if let var as? == if then else statement
+            self.messageLabel.text = nextString
+           // self.messageLabel.adjustsFontSizeToFitWidth = true
+        }
+        index = index + 1
+        index %= self.messageArray.count
+    }
+    
 }
 
